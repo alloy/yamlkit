c4d36cb0616339b7ef6d2b33a538080b4474e81e
Eloy Duran <eloy.de.enige@gmail.com>
2009-01-08 15:35:55 +0100

    Added YAML.load_file, does not pass the test yet, but _had_ to kill those tabs.

--- macruby/test/load_test.rb
+++ macruby/test/load_test.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env macruby
 
 ROOT = File.expand_path("../../../", __FILE__)
+FIXTURES = File.expand_path("../fixtures", __FILE__)
+
 framework File.join(ROOT, "build", "Release", "YAMLKit.framework")
+require File.join(ROOT, 'macruby', 'lib', 'yaml')
 
 require 'test/unit'
 class Test::Unit::TestCase
@@ -10,6 +13,20 @@ class Test::Unit::TestCase
       define_method("test_#{name}", &block)
     end
   end
+  
+  private
+  
+  def fixture(name)
+    File.join(FIXTURES, name)
+  end
 end
 
 # These tests should probably move to the macruby part of rubyspec once we get to that point.
+
+class TestYAML < Test::Unit::TestCase
+  it "loads a file with YAML.load_file" do
+    yaml = YAML.load_file(fixture('very_simple.yaml'))
+    expected = { 'title' => 'Escape of the Unicorn' }
+    assert_equal expected, yaml
+  end
+end
